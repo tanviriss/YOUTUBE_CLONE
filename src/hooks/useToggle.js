0b3d73688d5447e4d44a9f1521c8a3b80a579d4d
Tanvir <Tanvirislamnyc@gmail.com>
2024-04-19 22:09:10 -0400

    added all the components, fully functional

---
+++ src/hooks/useToggle.js
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+
+
+const useToggle = () => {
+  const [el, setEl] = useState(null);
+  const open = Boolean(el);
+  const handleClick = (event) => {
+    setEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setEl(null);
+  };
+  return {
+    el,
+    open,
+    handleClick,
+    handleClose,
+  };
+};
+export default useToggle;
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import './index.css';
+import App from './App';
+import AppThemeProvider from '@themes/AppThemeProvider';
+import CssBaseline from '@mui/material/CssBaseline';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <CssBaseline />
+    <AppThemeProvider>
+      <App />
+    </AppThemeProvider>
+  </React.StrictMode>
+);
+
+
+reportWebVitals();
